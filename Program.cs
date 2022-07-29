53cdce31b84143302f1ea36db4cb144bf2a818bb
Vladislav <vlad24500@mail.ru>
2022-07-29 11:03:51 +0300

    код готов

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void Print(string[] arr)
+{
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+string[] mass = { "hello", "2", "world", ":-)" };
+
+Print(mass);
+
+string[] result = new string[mass.Length];
+int j = 0;
+
+for (int i = 0; i < mass.Length; i++)
+{
+    if (mass[i].Length < 4)
+    {
+        result[j] = mass[i];
+        j++;
+    }
+}
+Array.Resize(ref result, j);
+Print(result);
